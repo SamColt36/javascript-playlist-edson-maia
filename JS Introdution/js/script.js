d7625d190d971ed0f30e409eebe4542858313035
SamColt36 <samuelss.contato@gmail.com>
2023-05-18 15:08:59 -0300

    Aula 1

---
+++ JS Introdution/js/script.js
@@ -0,0 +1,6 @@
+function alterarValor() {
+	document.getElementById("demo2").innerHTML = "Hello JavaScript!";
+}
+function color() {
+	document.getElementById("demo1").style.backgroundColor = "#5eeea7";
+}
