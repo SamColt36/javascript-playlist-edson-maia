45b9b0d8142b957b5d2cc57e85ebe908bfd76c74
SamColt36 <samuelss.contato@gmail.com>
2023-05-23 07:23:27 -0300

    Update class

---
+++ Arrays/script.js
@@ -0,0 +1,12 @@
+document.getElementById("demo").style.backgroundColor = "#1F1F1F" 	
+// Criando arrays
+let produtos = ['TV', 'Tablet', 'Microcomputer']
+var code = Array('Primeiro', 'Segundo', 'Terceiro')
+
+// Adicionar elementos no final com push = empurrar 
+produtos.push('Iphone')
+
+// Remover mentos no final com com pop = estourar
+// pop() é um método que não recebe parâmetros
+produtos.pop()
+
