55874ae90fc3d9fd2e474f964b48b5f43533fd3f
SamColt36 <samuelss.contato@gmail.com>
2023-05-24 17:15:38 -0300

    Update class

---
+++ Functions/script.js
@@ -0,0 +1,11 @@
+/*
+function name(parameter1, parameter2, parameter3) {
+	 code to be executed
+}
+*/
+let x = function1(2, 3)
+alert(x)
+function function1(var1, var2) {
+	return var1 ** var2;
+}
+document.
