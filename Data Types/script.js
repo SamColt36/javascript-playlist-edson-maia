acee3997ef628caac1b5969ed6ace0120ce39be1
SamColt36 <samuelss.contato@gmail.com>
2023-05-24 15:39:47 -0300

    Update class

---
+++ Data Types/script.js
@@ -0,0 +1,38 @@
+// Numbers:
+let numero1 = 16
+let numero2 = 7.5
+
+// Strings:
+let cor = "Yellow"
+let nome = "Johnson"
+
+// Booleans
+let verdadeiro = true
+let falso = false
+
+// Object:
+// OBS: A notação usada para instanciar um objeto é com uso de chaves diferente dos arrays que é usado colchetes
+const pessoa = { firstName: "John", lastName: "Doe" }
+
+// Array object:
+const listaCarros = ["Saab", "Volvo", "BMW"]
+
+// Date object:
+const date = new Date("2022-03-25")
+
+// O JS avalia as expressões lefet to right, logo o resultado de:
+// let x = 15 + 2 + "nome"
+// será a String "17nome"
+
+// Uma mesma variável pode ser usada com diferentes tipos de dados
+let x = 12
+x = true
+x = "String ramdom"
+// Ao usar um comand de output, p.ex. alert(x) o resultado será 'String ramdom'
+
+// Notação exponencial
+let exponencial1 = 123e5 //12300000
+let exponencia2 = 123e-5 // 0.00123
+
+// BigInt é um tipo de dao para repressentar numéros grandes. Similar ao Longer
+const PI = 3.141592653589793
