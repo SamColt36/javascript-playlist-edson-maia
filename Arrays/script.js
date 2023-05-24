2080964533d4ecc55997dfb3dd45fe4a7ae8b487
SamColt36 <samuelss.contato@gmail.com>
2023-05-24 10:28:45 -0300

    Update class

--- Arrays/script.js
+++ Arrays/script.js
@@ -4,9 +4,21 @@ let produtos = ['TV', 'Tablet', 'Microcomputer']
 var code = Array('Primeiro', 'Segundo', 'Terceiro')
 
 // Adicionar elementos no final com push = empurrar 
-produtos.push('Iphone')
+produtos.push('Iphone', 'HDD')
 
 // Remover mentos no final com com pop = estourar
 // pop() é um método que não recebe parâmetros
 produtos.pop()
+
+// Adicionar elementos no início do array
+produtos.unshift('DOM', 'REL')
+
+// Remover elementos no início do array
+produtos.shift()
+
+// Remover de uma posição específica
+// splice = emendar
+// sintaxe: (posição inicial, quantos remover a partir da posição inicial)
+produtos.splice(0,2)
+
 
