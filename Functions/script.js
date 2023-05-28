796d046cb838a6816d03e39aa7e6ee80bf03ac38
SamColt36 <samuelss.contato@gmail.com>
2023-05-28 10:37:15 -0300

    Update class

--- Functions/script.js
+++ Functions/script.js
@@ -21,6 +21,25 @@ function soma2(a, b) {
 olaMundo1() // procedimentos
 document.write(olaMundo2()) // funções
 document.write(`<hr>`)
-document.write(`<p> ${soma1(1, 2)}<br> </p>`)
+document.write(`<p> ${soma2(1, 2)}<br> </p>`)
 document.write(`<p> ${soma2(23, 65)}<br> </p>`)
-document.write()
+
+/* 
+Função anônima = não tem nome
+pode ou não ter parâmetros
+pode ou não ter retorno
+*/
+// É usada geralmente quando se quer passar uma função como parâmetro de outra função
+let titulo = document.querySelector('h1')
+titulo.addEventListener('click', function () { console.log('Você clicou no título') })
+
+// Arrow function ES 2015  em diante
+// Arrow function é uma função anônima (que não possui nome), não possui 'return' e nem 'function'
+/* 
+Geralmente é declarada através de uma 'const' cujo nome seria o nome da function seguido por space e um sinal de '=>' e as chaves como um elemento opcional 
+*/
+const nomeDaFunction = () => {
+	//procedimento
+	document.write('Olá mundo usando Arrow Funcion')
+}
+nomeDaFunction()
