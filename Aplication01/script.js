95ead6dcf1f1e1f1af858974e2169fabd89ce0bc
SamColt36 <samuelss.contato@gmail.com>
2023-06-02 20:30:42 -0300

    Update class

---
+++ Aplication01/script.js
@@ -0,0 +1,11 @@
+// Seleção dos elementos (input) do Form
+let boxName = document.querySelector('#nome'),
+	boxIdade = document.querySelector('#idade'),
+	boxPeso = document.querySelector('#peso'),
+	boxAltura = document.querySelector('#altura'),
+	boxImc = document.querySelector('#resultadoImc'),
+	resultImc = document.querySelector('#resultadoImc'),
+
+// Seleção dos buttons
+	buttonSubmit = document.querySelector('#btnEnviar'),
+	buttonReset = document.querySelector('#btnLimpar')
