4d80bb5d440dd9f8e3b4c30a808117db0697acc6
SamColt36 <samuelss.contato@gmail.com>
2023-06-03 11:04:23 -0300

    Update class

---
+++ Aplication02/script.js
@@ -0,0 +1,37 @@
+// PRImediaEIRO SE FAZ A SELEÇÃO DOS ELEMENTOS
+let nota1 = document.querySelector('#nota1'),
+	nota2 = document.querySelector('#nota2'),
+	calc = document.querySelector('#btnCalcular'),
+	reset = document.querySelector('#btnLimpar'),
+	media = document.querySelector('#media'),
+	situacao = document.querySelector('#situacao'),
+	aviso = document.querySelector('#aviso')
+
+calc.addEventListener('click', function (e) {
+	let n1 = nota1.value,
+		n2 = nota2.value,
+		med = ((n1 + n2) / 2)
+		media.value = med
+	aviso.textContent = sit(med)
+
+	console.log(n1)
+	console.log(n2)
+	console.log(med)
+	e.preventDefault()
+})
+
+function sit(med) {
+	let s = ""
+
+	if (med < 6) {
+		s = "Reprovado"
+	}
+	else if (6 <= med < 7) {
+		s = "AF"
+	}
+	else {
+		s = "Aprovado"
+	}
+	return s
+}
+
