c7ae767b8c82646d0cc7fc62f392bbdae675e203
SamColt36 <samuelss.contato@gmail.com>
2023-06-20 17:39:24 -0300

    Update class

--- Arrays/script.js
+++ Arrays/script.js
@@ -6,12 +6,13 @@ var code = Array('Primeiro', 'Segundo', 'Terceiro')
 // Adicionar elementos no final com push = empurrar 
 produtos.push('Iphone', 'HDD')
 
-// Remover mentos no final com com pop = estourar
+// Remover elementos no final com com pop = estourar
 // pop() é um método que não recebe parâmetros
-// produtos.pop()
+produtos.pop()
 
 // Adicionar elementos no início do array
 produtos.unshift('DOM', 'REL')
+
 
 // Remover elementos no início do array
 // produtos.shift()
@@ -20,6 +21,7 @@ produtos.unshift('DOM', 'REL')
 // splice = emendar
 // sintaxe: (posição inicial, quantos remover a partir da posição inicial)
 produtos.splice(0,2)
+
 
 // Copiar array slice = fatiar porção
 // sintaxe: (posição inicial, quantos copiar a partir da posição inicial)
@@ -35,5 +37,5 @@ let produtosAdendo = [...produtos, 'Mouse', 'Monitor']
 // Acessar posição de array
 // produtos[0]
 // Alterar valor de posição de array
-produtos[2] = 'Valor Alterado'
+produtos[2] = 'Valor Alterado usando produtos[2]'
 
