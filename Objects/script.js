009dda1d8ab71fec62a556c658657721d621ba3d
SamColt36 <samuelss.contato@gmail.com>
2023-06-20 18:13:28 -0300

    Update class

--- Objects/script.js
+++ Objects/script.js
@@ -11,5 +11,12 @@ function imc () {
 	let result =  (eu.peso / (Math.pow(eu.altura, 2)))
 	return result
 }
-document.write(`<h1> Nome completo ${eu.nomeCompleto()} </h1>`)
-document.write(`<h1> Nome completo ${imc().toFixed(2)} </h1>`)
+document.write(`<h1> Nome completo: ${eu.nomeCompleto()} </h1>`)
+
+// tofixed()
+// O método .toFixed(number) limita o número de casas decimais 
+document.write(`<h1> Imc: ${imc().toFixed(2)} </h1>`)
+
+// Alterar propriedade do objeto
+eu.nome = "Sam Colt 1836"
+document.write(`<h1> Nome atualizado: ${eu.nome} </h1>`)
