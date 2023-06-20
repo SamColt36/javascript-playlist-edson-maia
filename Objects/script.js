9d182a7213dab5a5f3c3341f68cfe054b8ee271f
SamColt36 <samuelss.contato@gmail.com>
2023-06-20 17:59:19 -0300

    Update class

--- Objects/script.js
+++ Objects/script.js
@@ -1,8 +1,15 @@
-let objetoQualquer = {
+let eu = {
 	nome: "Sam",
 	sobrenome: "Colt",
 	idade: 22,
+	peso: 51,
+	altura: 1.76,
 	// também é possível adicionar função dentro do elemento do objeto
 	nomeCompleto: function () { return this.nome + " " + this.sobrenome }
 }
-document.getElementById("demo").innerHTML = objetoQualquer.nomeCompleto()
+function imc () {
+	let result =  (eu.peso / (Math.pow(eu.altura, 2)))
+	return result
+}
+document.write(`<h1> Nome completo ${eu.nomeCompleto()} </h1>`)
+document.write(`<h1> Nome completo ${imc().toFixed(2)} </h1>`)
