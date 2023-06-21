6a07be53ecd8045535dc48206619e049a2b76567
SamColt36 <samuelss.contato@gmail.com>
2023-06-21 16:28:36 -0300

    Update class

--- Example01/script.js
+++ Example01/script.js
@@ -1,1 +1,16 @@
-let btnAntes = document.querySelector('#antes')
+let box = document.querySelector('.box')
+let btnAvancar = document.querySelector('#depois')
+btnAvancar.addEventListener('click', avancar)
+let btnVoltar = document.querySelector('#antes')
+btnVoltar.addEventListener('click', voltar)
+
+function avancar() {
+	console.log('avancar 300px')
+	box.classList.add('avancar')
+	box.classList.remove('voltar')
+}
+function voltar() {
+	console.log('voltar 300px')
+	box.classList.remove('avancar')
+	box.classList.add('voltar')
+}
