22731bfb0376d52b6f9fc99f46b1c48ff797e30c
SamColt36 <samuelss.contato@gmail.com>
2023-06-26 06:46:53 -0300

    Update class

--- CalcularImc/script.js
+++ CalcularImc/script.js
@@ -24,14 +24,11 @@ MAS, ANTES DETERMINE UM EVENTO COMO REFERENCIA PARA PEGAR OS DADOS */
 btnEnviar.addEventListener('click', function (e) {
 	// pegar os values de cada input
 	let nome = cxNome.value
-	let idade = cxIdade.value
-	let peso = cxPeso.value
-	let altura = cxAltura.value
+	let idade = parseInt(cxIdade.value)
+	let peso = parseFloat(cxPeso.value)
+	let altura = parseFloat(cxAltura.value)
 	let imc = (peso / (altura * altura)).toFixed(1)
-
-	cxImc.value = imc
 	let sit = situacaoDoPeso(imc)
-	aviso.textContent = sit
 
 	// criar objeto pessoa
 	let pessoa = {
@@ -44,10 +41,21 @@ btnEnviar.addEventListener('click', function (e) {
 	}
 
 	dados[1].textContent = pessoa.nome
-	dados[2].textContent = pessoa.idade + " anos"
-	dados[3].textContent = pessoa.peso + " kg"
-	dados[4].textContent = pessoa.altura + " m"
-	dados[5].textContent = pessoa.imc + " " + pessoa.sit
+	if (isNaN(pessoa.peso) || isNaN(pessoa.altura)) {
+		cxImc.value = imc
+		aviso.textContent = 'Informe seu peso corretamente'
+	} else {
+		// Rotina que verifica se o campo idade tem conteúdo
+		// Se não tiver conteúdo ele não imprimi nada da saída
+		if (isNaN(pessoa.idade)) dados[2].textContent = ''
+		// Se tiver conteúdo imprimi o resultado mais uma String
+		else dados[2].textContent = pessoa.idade + " anos"
+		
+		aviso.textContent = sit
+		dados[3].textContent = pessoa.peso + " kg"
+		dados[4].textContent = pessoa.altura + " m"
+		dados[5].textContent = pessoa.imc + " " + pessoa.sit
+	}
 
 	// previnir o comportamento padrao
 	e.preventDefault()
@@ -68,7 +76,30 @@ function situacaoDoPeso(imc) {
 	} else if (imc >= 40) {
 		situacao = 'Obesidade III'
 	} else {
-		situacao = 'Informe seu peso corretamente'
+		situacao = ''
 	}
 	return situacao
 }
+
+function reset() {
+	aviso.textContent = ''
+}
+
+function validar(num) {
+	if (num.value < 0) {
+		aviso.textContent = 'Entrada inválida!'
+
+		setTimeout(function () {
+			num.value = ''
+			reset()
+		}, 2000)
+	}
+}
+// Como o button reset do HTML já reseta os campos de entrada, não é necessários adicionar js para isso
+// Porém, para resetar a tabela com as informações do usuário foi necessário adicionar a lógica abaixo
+btnLimpar.addEventListener('click', function () {
+	reset()
+	for (let i = 0; i < dados.length; i++) {
+		dados[i].textContent = ''
+	}
+})
