a2f2f129c49cab4432f79857eb3d716979f0ace2
SamColt36 <samuelss.contato@gmail.com>
2023-07-03 18:43:11 -0300

    Update class

--- DadoD6/script.js
+++ DadoD6/script.js
@@ -1,14 +1,15 @@
 // 	Seleção dos elementos
 let numeroSorteado = 0
-let	sorteado = document.querySelector('#sorteado')
-let	imgDado = document.querySelector('#imgDado')
-let	btnSortear = document.querySelector('#btnSortear')
-let	audio = document.querySelector('#dadoRolando')
+let	sorteado = document.getElementById('sorteado')
+let	imgDado = document.getElementById('imgDado')
+let	btnSortear = document.getElementById('btnSortear')
+let	audio = document.getElementById('dadoRolando')
 
 
 btnSortear.addEventListener('click', function () {
 	imgDado.classList.add('animar')
 	sorteado.classList.add('aparecer')
 	audio.play()
-	btnSortear.sty
+	btnSortear.style.display = 'none'
+	console.log('casa')
 })
