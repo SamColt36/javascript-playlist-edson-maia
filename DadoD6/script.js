11004a6010c64403615dee12a723dddaa529c0e4
SamColt36 <samuelss.contato@gmail.com>
2023-07-05 19:48:16 -0300

    Update class

--- DadoD6/script.js
+++ DadoD6/script.js
@@ -1,13 +1,31 @@
 // 	Seleção dos elementos
 let numeroSorteado = 0
-let	sorteado = document.getElementById('sorteado')
-let	imgDado = document.getElementById('imgDado')
-let	btnSortear = document.getElementById('btnSortear')
-let	audio = document.getElementById('dadoRolando')
+let sorteado = document.getElementById('sorteado')
+let imgDado = document.getElementById('imgDado')
+let btnSortear = document.getElementById('btnSortear')
+let audio = document.getElementById('dadoRolando')
 
 
-btnSortear.addEventListener('click', function() {
+btnSortear.addEventListener('click', function () {
 	imgDado.classList.add('animar')
-	imgDado.classList.add('aparecer')
-	console.log('casa')
+	sorteado.classList.add('aparecer')
+	audio.play()
+	btnSortear.style.display = 'none'
+
+	setTimeout(function () {
+		numeroSorteado = getRandomInt(1, 6)
+		console.log(numeroSorteado)
+		imgDado.setAttribute('src', 'images/' + numeroSorteado + '.png')
+		sorteado.textContent = numeroSorteado
+		btnSortear.style.display = 'inline-block'
+		imgDado.classList.remove('animar')
+		sorteado.classList.remove('aparecer')
+
+	}, 1750)
 })
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min); // arredonda para o int acima
+	max = Math.floor(max); // arredonda para o int abaixo
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
