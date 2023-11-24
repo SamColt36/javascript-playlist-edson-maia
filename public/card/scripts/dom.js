dc7a366f4060aba97da6f8e1c3a1836029349f2f
SamColt36 <samuelss.contato@gmail.com>
2023-11-24 16:57:12 -0300

    feat: inserindo json

---
+++ public/card/scripts/dom.js
@@ -0,0 +1,11 @@
+const conteudos = $('.conteudos')
+const card = $('.card')
+const foto = $('#foto')
+const nome = $('#nome')
+const nacionalidade = $('#nacionalidade')
+const idade = $('#idade')
+const peso = $('#peso')
+const altura = $('#altura')
+
+
+
